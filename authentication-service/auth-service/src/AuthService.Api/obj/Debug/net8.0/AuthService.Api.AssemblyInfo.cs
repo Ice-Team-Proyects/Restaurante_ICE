f39e63023e3bf8e2815ed1ffe496acf0d3a17945
rgodinez-2024247 <rgodinez-2024247@kinal.edu.gt>
2026-06-19 17:20:08 -0600

    fix(BFC-036): corrige configuracion de Docker para levantar el proyecto

--- authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8414d3e7992174bafd724c2fc2aa1905a75057f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86dfc5480411a3aa8ee51d5b4f125727a6f8945a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
